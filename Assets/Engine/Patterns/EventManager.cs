688b1e469aa631acab6de2349cbc748f1df2d6d2
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-16 15:58:54 -0300

    EventManager

---
+++ Assets/Engine/Patterns/EventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager {
+    public delegate void Event();
+
+    public static Dictionary<int, Event> events = new Dictionary<int, Event>();
+
+    public static void AddEventListener(int id,Event function)
+    {
+        if (events.ContainsKey(id))
+            events[id] += function;
+        else
+            events.Add(id, function);
+    }
+	
+    public static void DispathEvent(int id)
+    {
+        events[id]();
+    }
+}
