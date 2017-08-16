42aa624d0c8a26d82f85e35b4d35e3d7731c7602
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-16 15:29:31 -0300

    Pool With Factory

---
+++ Assets/Game/FactoryBullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FactoryBullet : SingletonClass<FactoryBullet>, ObjectPool<Bullet>.IFactory
+{
+    public Bullet prefab;
+    public int initialStock;
+    ObjectPool<Bullet> pool;
+
+    public override void Awake()
+    {
+        base.Awake();
+        pool = new ObjectPool<Bullet>(initialStock, this, true);
+    }
+
+    public Bullet CraeteMethod()
+    {
+        Bullet b = Instantiate(prefab);
+        b.transform.SetParent(transform);
+        return b;
+    }
+
+    public void DisableMethod(Bullet obj)
+    {
+        obj.gameObject.SetActive(false);
+    }
+
+    public void EnabledMethod(Bullet obj)
+    {
+        obj.gameObject.SetActive(true);
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,38 +5,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    ObjectPool<int> integers;
-    static int i;
-	void Start () {
-        integers = new ObjectPool<int>(5, ci, di, true);
-        integers.GetObject();
-	}
-
-    private void di(int obj)
-    {
-        print("Store : " + obj);
-    }
-
-    private void ei(int obj)
-    {
-        print(obj);
-    }
-
-    private int ci()
-    {
-        return i++;
-    }
-
     void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine(DestroyMe(FactoryBullet.instance.GetBullet()));
+           // StartCoroutine(DestroyMe(FactoryBullet.instance.GetBullet()));
         }	
 	}
 
-    private IEnumerator DestroyMe(Bullet b)
-    {
-        yield return new WaitForSeconds(3);
-        FactoryBullet.instance.ReturnBullet(b);
-    }
+   
 }
