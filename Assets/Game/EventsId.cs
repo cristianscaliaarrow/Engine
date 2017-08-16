73dbbb2f3c2ab06f29aa7002fbea56bc2d487f31
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-16 16:02:35 -0300

    TestEventManager

---
+++ Assets/Game/EventsId.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsId : MonoBehaviour {
+    public const int GameStart = 0;
+    public const int GamePause = 1;
+    public const int GameEnd = 2;
+
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,12 +5,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    private void Start()
+    {
+        EventManager.AddEventListener(EventsId.GameStart, OnStart);
+        EventManager.AddEventListener(EventsId.GamePause, OnPausa);
+        EventManager.AddEventListener(EventsId.GameEnd, OnEnd);
+    }
+
+    private void OnEnd()
+    {
+        print("OnEnd");
+    }
+
+    private void OnPausa()
+    {
+        print("OnPausa");
+    }
+
+    private void OnStart()
+    {
+        print("OnStart");
+    }
+
     void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Bullet b = FactoryBullet.Pool;
-        }	
-	}
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            EventManager.DispathEvent(EventsId.GameStart);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            EventManager.DispathEvent(EventsId.GamePause);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            EventManager.DispathEvent(EventsId.GameEnd);
+        }
+    }
 
    
 }
